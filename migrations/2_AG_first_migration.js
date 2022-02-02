993666cdacc5eab7661939dc9dbdd8d3762c77a2
joYyHack <agalaktionov5490@gmail.com>
2022-02-02 08:06:56 +0200

    Init Smart Contract

--- migrations/2_AG_first_migration.js
+++
@@ -1,5 +0,0 @@
-const AGFirstContract = artifacts.require("AGFirstContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(AGFirstContract);
-};
--- migrations/3_AG_second_migration.js
+++
@@ -1,5 +0,0 @@
-const AGFirstContract = artifacts.require("AGSecondContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(AGFirstContract);
-};
