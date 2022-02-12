08f76988b56cc6b3a51462594d1d994c88b3f2ac
joYyHack <agalaktionov5490@gmail.com>
2022-02-12 09:05:15 +0200

    Update .gitignore

--- client/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-//const MigrationsContractAddr = "0x385CaFA5715bC9cE2487A555b1C781238B23D101";
-
-import contractInJson from "../../../smart-contracts/build/contracts/Transactions";
-
-export const TransactionsContractAddr = "0x5f0EbD31C35F00Ac894C2f4AC88CCE938100B6E2";
-export const TransactionsABI = contractInJson.abi;
